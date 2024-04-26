31155da970b6178246201e21d981c378d687cf17
Thomas Hennes <jaxxeh@gmail.com>
2024-04-26 18:09:51 +0200

    Initial workflow commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Dockerized SvelteKit</h1>
+<p>Hello, World!</p>
+<a href="/about">About</a>
